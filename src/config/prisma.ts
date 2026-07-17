5b39854a3bba2b24175f55507be46dac1804dcb3
khauee <khauebraga55@gmail.com>
2026-07-17 10:26:16 -0300

    feat: configuracao final do prisma 7.8 e criacao das tabelas usuario e vinculo

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,3 +1,9 @@
-import { PrismaClient } from '@prisma/client';
+import { defineConfig } from '@prisma/config';
+import 'dotenv/config';
+import process from 'process'; // A importação que resolveu o erro do TS!
 
-export const prisma = new PrismaClient();
+export default defineConfig({
+  datasource: {
+    url: process.env.DIRECT_URL as string,
+  }
+});
