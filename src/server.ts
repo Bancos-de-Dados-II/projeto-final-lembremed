e846b9c960337408b3cca5ed560cfdee13ceb0ac
João Lucas <joaolucasmoraisdossantos@gmail.com>
2026-07-25 02:33:26 -0300

    feat: registra rotas de ponto de saúde e alerta de emergencia

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import 'dotenv/config';
 import { usuarioRoutes } from './routes/usuario.routes';
 import { vinculoRoutes } from './routes/vinculo.routes';
+import { pontoSaudeMapaRoutes } from './routes/pontoSaudeMapa.routes';
+import { alertaEmergenciaRoutes } from './routes/alertaEmergencia.routes';
 
 // Importando as conexões da infraestrutura poliglota
 import conectarMongoDB from './config/mongo';
@@ -11,7 +13,7 @@ import { conectarRedis } from './config/redis';
 const app = express();
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 // --- CONECTANDO AOS BANCOS NÃO-RELACIONAIS ---
 conectarMongoDB();
@@ -20,8 +22,10 @@ conectarRedis();
 // --- CONECTANDO AS ROTAS ---
 app.use('/usuarios', usuarioRoutes); // <-- 1. TODA ROTA DE USUÁRIO PASSA POR AQUI
 app.use('/vinculos', vinculoRoutes); // <-- 2. TODA ROTA DE VÍNCULO PASSA POR AQUI
+app.use('/pontos-saude-mapa', pontoSaudeMapaRoutes);
+app.use('/alertas-emergencia', alertaEmergenciaRoutes);
 
-// Rota de Teste 
+// Rota de Teste
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'API LembreMed rodando perfeitamente! 🚀' });
 });
