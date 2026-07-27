720ad3de0fda4ea3c423a77f90c8d4f20a8907fe
fefe06-dev <felicia.anna@academico.ifpb.edu.br>
2026-07-27 09:41:30 -0300

    Feat: Correção do layout e renderização de mapa

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="app-shell">
       <MeusRemedios />
-      <MapaFarmacias />
+      <div className="secao-mapa">
+        <MapaFarmacias />
+      </div>
     </div>
   );
 }
